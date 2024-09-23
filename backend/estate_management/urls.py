ea4e46d3988eb5ff4b082255d82476c899569f11
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:42:16 +0100

    Update urls.py

--- backend/estate_management/urls.py
+++ backend/estate_management/urls.py
@@ -1,3 +1,6 @@
+from apps.payments.views import PaymentViewSet
+
+router.register(r'payments', PaymentViewSet)
 from apps.properties.views import PropertyViewSet
 
 router.register(r'properties', PropertyViewSet)
