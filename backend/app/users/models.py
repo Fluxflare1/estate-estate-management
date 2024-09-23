585b2e37c056cfb23a093c43c68e0f7caaf2ae8b
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 00:12:11 +0100

    Update models.py

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,4 +1,11 @@
 # backend/apps/users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+# backend/apps/users/models.py
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
