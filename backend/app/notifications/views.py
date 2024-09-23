78b6d19fbc15fd3c8067d74f4b5c166e85fb5a51
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 20:01:44 +0100

    Create views.py

---
+++ backend/app/notifications/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Notification
+from .serializers import NotificationSerializer
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return self.queryset.filter(user=user)
