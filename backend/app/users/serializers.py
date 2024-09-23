4daa94989fc399e8f436611f7e079441ca4694a1
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:22:51 +0100

    Update serializers.py

--- backend/app/users/serializers.py
+++ backend/app/users/serializers.py
@@ -4,4 +4,11 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ['id', 'username', 'email', 'is_property_owner', 'is_tenant', 'is_agent', 'is_admin']
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
