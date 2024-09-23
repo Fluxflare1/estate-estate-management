8a5d383c5306012508a0c3e037c22f7045df6bf8
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 21:20:54 +0100

    Create Receipt.js

---
+++ frontend/src/components/Receipt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Receipt = ({ receipt }) => {
+    return (
+        <div>
+            <h2>Receipt</h2>
+            {/* Display receipt details */}
+        </div>
+    );
+};
+
+export default Receipt;
