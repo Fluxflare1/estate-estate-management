6404d15afec4661176afa6b01287c882b34d02cc
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 19:12:17 +0100

    Update views.py

--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -1,3 +1,32 @@
+# backend/apps/users/views.py
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+class LoginUserView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = User.objects.filter(email=email).first()
+
+        if user and user.check_password(password):
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({"error": "Invalid credentials"}, status=400)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
