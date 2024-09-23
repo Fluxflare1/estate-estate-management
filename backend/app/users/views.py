a847df421c6b4917aff2938924ae1eea35642d7f
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 00:14:07 +0100

    Update views.py

--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -1,3 +1,18 @@
+# backend/apps/users/views.py
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserSerializer
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(id=self.request.user.id)
+
+    def perform_update(self, serializer):
+        serializer.save()
 # apps/users/views.py
 
 from rest_framework import viewsets
