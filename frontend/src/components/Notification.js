bcfbb877f3876fda38025ff207addbb43a7e1eae
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 20:05:26 +0100

    Create Notification.js

---
+++ frontend/src/components/Notification.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Notification = () => {
+    const [notifications, setNotifications] = useState([]);
+
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            const response = await axios.get('/api/notifications/');
+            setNotifications(response.data);
+        };
+        fetchNotifications();
+    }, []);
+
+    return (
+        <div>
+            <h2>Notifications</h2>
+            <ul>
+                {notifications.map((notification) => (
+                    <li key={notification.id}>{notification.message}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Notification;
