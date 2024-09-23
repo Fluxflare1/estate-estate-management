1c055ebef6709e92344f17c53b334a021cbdde7c
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:21:19 +0100

    Update models.py

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_property_owner = models.BooleanField(default=False)
+    is_tenant = models.BooleanField(default=False)
+    is_agent = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 from django.db import models
 
 class User(models.Model):
