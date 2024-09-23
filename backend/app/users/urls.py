d1f59e53c1796f1b97119ece67ec11383d64cd4e
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 19:13:15 +0100

    Update urls.py

--- backend/app/users/urls.py
+++ backend/app/users/urls.py
@@ -1,3 +1,11 @@
+# backend/apps/users/urls.py
+from django.urls import path
+from .views import RegisterUserView, LoginUserView
+
+urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login'),
+]
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from apps.users.views import UserViewSet
