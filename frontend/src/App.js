6ff08150a71dde42487dc98087eb30e1de3ce9ee
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:03:27 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import UserComponent from './components/UserComponent';
+import PropertyComponent from './components/PropertyComponent';
 
+const App = () => {
+    return (
+        <div>
+            <UserComponent />
+            <PropertyComponent />
+        </div>
+    );
+};
+
+export default App;
