973209bd0b4654155956dc41603141b1d40ea22f
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:27:25 +0100

    Update views.py

--- backend/app/properties/views.py
+++ backend/app/properties/views.py
@@ -5,3 +5,10 @@ from .serializers import PropertySerializer
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+from rest_framework import viewsets
+from .models import Property
+from .serializers import PropertySerializer
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
