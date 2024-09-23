c2f47bc187597c46c3c8cf93871617ec59c11c8f
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 19:41:26 +0100

    Update views.py

--- backend/app/payments/views.py
+++ backend/app/payments/views.py
@@ -1,3 +1,15 @@
+# backend/apps/payments/views.py
+from rest_framework import generics
+from .models import Payment
+from .serializers import PaymentSerializer
+
+class PaymentListCreateView(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class PaymentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
 from rest_framework import viewsets
 from .models import Payment
 from .serializers import PaymentSerializer
