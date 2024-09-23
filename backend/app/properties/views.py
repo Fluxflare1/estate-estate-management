39ade4de9ca80e742e5af8ae8d15f237f7b5188c
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 00:36:17 +0100

    Update views.py

--- backend/app/properties/views.py
+++ backend/app/properties/views.py
@@ -1,3 +1,15 @@
+# backend/apps/properties/views.py
+from rest_framework import viewsets, permissions
+from .models import Property
+from .serializers import PropertySerializer
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(landlord=self.request.user)
 from rest_framework import viewsets
 from .models import Property
 from .serializers import PropertySerializer
