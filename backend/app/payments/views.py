b334962cda26c060526eedddd16f1baf32235148
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:41:35 +0100

    Update views.py

--- backend/app/payments/views.py
+++ backend/app/payments/views.py
@@ -5,3 +5,10 @@ from .serializers import PaymentSerializer
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+from rest_framework import viewsets
+from .models import Payment
+from .serializers import PaymentSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
