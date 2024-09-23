f4fe9d88ce99aff559057f5c72d7ff19fbcab474
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 19:28:44 +0100

    Update views.py

--- backend/app/properties/views.py
+++ backend/app/properties/views.py
@@ -1,3 +1,15 @@
+# backend/apps/properties/views.py
+from rest_framework import generics
+from .models import Property
+from .serializers import PropertySerializer
+
+class PropertyListCreateView(generics.ListCreateAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+class PropertyRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
 from rest_framework import viewsets
 from .models import Property
 from .serializers import PropertySerializer
