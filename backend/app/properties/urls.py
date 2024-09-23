8375a3dfab813274ece358f83e3eae37713c2a43
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 00:37:25 +0100

    Update urls.py

--- backend/app/properties/urls.py
+++ backend/app/properties/urls.py
@@ -1,3 +1,14 @@
+# backend/apps/properties/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PropertyViewSet
+
+router = DefaultRouter()
+router.register(r'properties', PropertyViewSet, basename='properties')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PropertyViewSet
