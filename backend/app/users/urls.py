7e7efbca13fe24dfe6afbad688e0427df98d7b18
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:24:37 +0100

    Update urls.py

--- backend/app/users/urls.py
+++ backend/app/users/urls.py
@@ -1,3 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from apps.users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.users.views import UserViewSet
