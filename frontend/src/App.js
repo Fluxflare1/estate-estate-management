57b3307c019aff897b7a075d278733f0432e837e
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:51:15 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserComponent from './components/UserComponent';
+import PropertyComponent from './components/PropertyComponent';
+import PaymentComponent from './components/PaymentComponent';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/users" component={UserComponent} />
+                <Route path="/properties" component={PropertyComponent} />
+                <Route path="/payments" component={PaymentComponent} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
+import React from 'react';
 import UserComponent from './components/UserComponent';
 import PropertyComponent from './components/PropertyComponent';
 import PaymentComponent from './components/PaymentComponent';
