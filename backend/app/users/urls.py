07655d8822f6ae7fa7eac00d96691d4f75bf510d
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 00:14:58 +0100

    Update urls.py

--- backend/app/users/urls.py
+++ backend/app/users/urls.py
@@ -1,4 +1,15 @@
 # backend/apps/users/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserProfileViewSet
+
+router = DefaultRouter()
+router.register(r'profile', UserProfileViewSet, basename='user-profile')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+# backend/apps/users/urls.py
 from django.urls import path
 from .views import RegisterUserView, LoginUserView
 
