a5af3d2a55cd623149a44be52e9d4cb1466309f3
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 18:32:42 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,14 @@
+useEffect(() => {
+    fetch('/api/users/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => setUsers(data))
+        .catch(error => console.error('There was a problem with the fetch operation:', error));
+}, []);
 import { Link } from 'react-router-dom';
 
 function Navbar() {
