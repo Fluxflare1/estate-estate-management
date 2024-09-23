5026b4036871b05765f1a2a340136c09bc406f00
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 17:26:26 +0100

    Update serializers.py

--- backend/app/properties/serializers.py
+++ backend/app/properties/serializers.py
@@ -5,3 +5,10 @@ class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = '__all__'
+from rest_framework import serializers
+from .models import Property
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = '__all__'
