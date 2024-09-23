3fe2ee06565374955ee1cdac4cce0fe58d539054
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 18:51:49 +0100

    Update views.py

--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -1,3 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
+
+class RegisterUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=201)
+        return Response(serializer.errors, status=400)
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
