86c86923abc8edb115f7003cfc0254e7a5ee85b8
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-23 18:48:35 +0100

    Update settings.py

--- backend/estate_management/settings.py
+++ backend/estate_management/settings.py
@@ -1,3 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estate_management',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
