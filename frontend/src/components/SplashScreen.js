0e9acfe5aa6656da7ab3d58a3b6955de063dfdc0
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 22:49:04 +0100

    Create SplashScreen.js

---
+++ frontend/src/components/SplashScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SplashScreen.css'; // Make sure to create this CSS file
+
+const SplashScreen = () => {
+    return (
+        <div className="splash-screen">
+            Welcome to the Estate Management System
+        </div>
+    );
+}
+
+export default SplashScreen;
