487ce51564a82faf6c55b573e2d92646649b9cc4
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 16:57:19 +0100

    Update task.py

--- backend/app/notifications/task.py
+++ backend/app/notifications/task.py
@@ -1,1 +1,11 @@
+from django.core.mail import send_mail
+from .models import Notification
 
+def send_notification_email(user_email, subject, message):
+    send_mail(
+        subject,
+        message,
+        'noreply@yourapp.com',
+        [user_email],
+        fail_silently=False,
+    )
