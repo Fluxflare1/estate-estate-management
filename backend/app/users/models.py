b514eb6ce76b718d87bcbfba0a33985268b957a7
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 14:03:34 +0100

    Update models.py

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 # backend/apps/users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
