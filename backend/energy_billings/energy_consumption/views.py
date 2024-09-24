93ee3076722981e635f23790d22e3a6ea0caeb96
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 19:57:13 +0100

    Update views.py

--- backend/energy_billings/energy_consumption/views.py
+++ backend/energy_billings/energy_consumption/views.py
@@ -1,3 +1,15 @@
+# backend/energy-billings/energy_consumption/views.py
+from rest_framework import viewsets, permissions
+from .models import EnergyBillingAccount
+from .serializers import EnergyBillingAccountSerializer
+
+class EnergyBillingAccountViewSet(viewsets.ModelViewSet):
+    queryset = EnergyBillingAccount.objects.all()
+    serializer_class = EnergyBillingAccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(landlord=self.request.user)
 from rest_framework import viewsets
 from .models import EnergyBillingAccount, MeterReading, EnergyPurchase
 from .serializers import EnergyBillingAccountSerializer, MeterReadingSerializer, EnergyPurchaseSerializer
