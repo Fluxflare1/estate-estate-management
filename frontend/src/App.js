cffee73b61a9d6f8519e8ada906a25530a5acd43
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 00:51:42 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,29 @@
+// src/App.js
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <PrivateRoute>
+                            <Home />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
 import React from 'react';
 import TenantsList from './components/TenantsList';
 import EnergyPurchasesList from './components/EnergyPurchasesList';
