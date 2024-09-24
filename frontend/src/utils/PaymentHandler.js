81b52948ee3ae4d1929f53d0bb8c2dd3521a0b80
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-24 23:05:30 +0100

    Create PaymentHandler.js

---
+++ frontend/src/utils/PaymentHandler.js
@@ -0,0 +1,8 @@
+const handlePayment = (amount) => {
+    if (!amount || amount <= 0) {
+        alert("Please enter a valid amount");
+        return;
+    }
+    // Proceed with payment processing
+    // API call to submit payment
+};
