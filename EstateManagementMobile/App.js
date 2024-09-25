2eba09dc74481f557f5574551ccc20916b335a03
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 08:23:25 +0100

    Update App.js

--- EstateManagementMobile/App.js
+++ EstateManagementMobile/App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import PropertyList from './components/PropertyList';
 
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="PropertyList" component={PropertyList} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
