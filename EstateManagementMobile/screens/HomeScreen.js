88b2f01070faee5530c214cf2f06324f5cc92b55
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 08:22:22 +0100

    Update HomeScreen.js

--- EstateManagementMobile/screens/HomeScreen.js
+++ EstateManagementMobile/screens/HomeScreen.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    const goToProperties = () => {
+        navigation.navigate('PropertyList');
+    };
+
+    return (
+        <View>
+            <Text>Welcome to the Estate Management App</Text>
+            <Button title="View Properties" onPress={goToProperties} />
+        </View>
+    );
+};
+
+export default HomeScreen;
