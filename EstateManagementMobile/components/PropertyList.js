e526b97819b18ab5e75cba81e7a63e238978045d
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 08:29:29 +0100

    Update PropertyList.js

--- EstateManagementMobile/components/PropertyList.js
+++ EstateManagementMobile/components/PropertyList.js
@@ -1,3 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+const PropertyList = () => {
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://your-api-endpoint.com/properties')
+            .then(response => {
+                setProperties(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching properties:', error);
+            });
+    }, []);
+
+    return (
+        <View>
+            {properties.map(property => (
+                <View key={property.id}>
+                    <Text>{property.name}</Text>
+                </View>
+            ))}
+        </View>
+    );
+};
+
+export default PropertyList;
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 
